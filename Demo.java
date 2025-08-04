4b37f5a1537a6b9ec60d8dee182023d38202034e
Arshitha-K <arshumadhu.4@gmail.com>
2025-08-04 17:27:44 +0530

    Class added

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public class Calculator{
+        public int add(int num1, int num2) {
+            return num1 + num2;
+        }
+    }
+    
+}
